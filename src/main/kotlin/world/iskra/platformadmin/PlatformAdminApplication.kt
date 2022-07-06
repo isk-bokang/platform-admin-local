6694b04bed6dbe790608f174c4f49b9d233e10ff
isk-bokang <bo.kang@iskra.world>
2022-07-06 11:56:34 +0900

    initial commit

---
+++ src/main/kotlin/world/iskra/platformadmin/PlatformAdminApplication.kt
@@ -0,0 +1,11 @@
+package world.iskra.platformadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlatformAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlatformAdminApplication>(*args)
+}
---
+++ src/test/kotlin/world/iskra/platformadmin/PlatformAdminApplicationTests.kt
@@ -0,0 +1,13 @@
+package world.iskra.platformadmin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlatformAdminApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
