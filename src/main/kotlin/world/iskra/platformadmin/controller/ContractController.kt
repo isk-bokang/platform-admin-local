f3c75299c2b7937e91732b196eeeeeac78355a5d
isk-bokang <bo.kang@iskra.world>
2022-07-09 15:15:07 +0900

    feat: Add ContractController

--- src/main/kotlin/world/iskra/platformadmin/controller/ContractController.kt
+++ src/main/kotlin/world/iskra/platformadmin/controller/ContractController.kt
@@ -1,4 +1,33 @@
 package world.iskra.platformadmin.controller
 
-class ContractController {
+import lombok.RequiredArgsConstructor
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import world.iskra.platformadmin.entity.Contract
+import world.iskra.platformadmin.service.IContractService
+
+@RestController
+@RequiredArgsConstructor
+class ContractController(
+    private val contractService: IContractService
+) {
+    @GetMapping("/contracts/")
+    fun getContracts(): ArrayList<Contract> {
+        return contractService.getContracts()
+    }
+
+    @GetMapping("/contracts/{contractId}")
+    fun getContract(@PathVariable contractId: String): Contract {
+        return contractService.getContract(contractId.toLong())
+    }
+
+    @PostMapping("/contracts")
+    fun registerContract(@RequestBody contract : Contract): Contract {
+        return contractService.registerContract(contract)
+    }
+
 }
