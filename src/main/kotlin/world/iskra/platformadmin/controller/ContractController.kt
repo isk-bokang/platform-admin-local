698fef04e7e245a4c72f69ffb6cde82304cdeda9
isk-bokang <bo.kang@iskra.world>
2022-07-12 16:05:20 +0900

    fix: fix wrong url (/contracts/ -> /contract)

--- src/main/kotlin/world/iskra/platformadmin/controller/ContractController.kt
+++ src/main/kotlin/world/iskra/platformadmin/controller/ContractController.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import world.iskra.platformadmin.entity.Contract
 import world.iskra.platformadmin.service.IContractService
@@ -15,7 +14,7 @@ import world.iskra.platformadmin.service.IContractService
 class ContractController(
     private val contractService: IContractService
 ) {
-    @GetMapping("/contracts/")
+    @GetMapping("/contracts")
     fun getContracts(): ArrayList<Contract> {
         return contractService.getContracts()
     }
